9ff49fb008e2a921954e0b4d763612b75883dea3
Prolay Mondal <prolaymondal047@gmail.com>
2026-07-18 18:10:01 +0530

    rename

---
+++ basics/datatypes.py
@@ -0,0 +1,17 @@
+string1 = "this is a string"
+int1 = 123
+float1 = 34.563576
+
+print(round(float1,2))
+print(f"{float1:.2f}")
+
+bool_true = True
+bool_false = False
+
+tuple1 = (1,2,3) #immutable, can have duplicated value and preserved order of the items
+print(tuple1[0])
+tuple_str = ("ha",2,3)
+print(tuple_str[0])
+
+set1 = {1,2,3,53,2,} # mutable, cannot have duplicate value and do not preserved order of the items
+print(set1)
---
+++ basics/printing.py
@@ -0,0 +1,6 @@
+print("Hey" + " stranger")
+print('''
+    Looking at my python repo, hmm
+    You are most welcome to my pytho repo
+''')
+print("How your programming journey going?\nHope it is going fun")
